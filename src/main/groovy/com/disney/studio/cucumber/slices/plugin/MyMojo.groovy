c2ae3ca0f1d647e90fff15059c04b7ea9125c432
Robert Stagner <robert.stagner@disney.com>
2016-09-29 10:37:53 -0700

    * removed all instances of Java and replace with Groovy code

--- src/main/groovy/com/disney/studio/cucumber/slices/plugin/MyMojo.groovy
+++ src/main/groovy/com/disney/studio/cucumber/slices/plugin/MyMojo.groovy
@@ -1,20 +1,4 @@
-package sample.plugin;
-
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package com.disney.studio.cucumber.slices.plugin
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -22,11 +6,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.plugins.annotations.ResolutionScope;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 
 /**
  * Goal which touches a timestamp file.
@@ -34,17 +14,17 @@ import java.io.IOException;
  * @deprecated Don't use!
  */
 @Mojo( name = "touch", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class MyMojo
-    extends AbstractMojo
+class MyMojo
+        extends AbstractMojo
 {
     /**
      * Location of the file.
      */
-    @Parameter( defaultValue = "${project.build.directory}", property = "outputDir", required = true )
+    @Parameter( defaultValue = '${project.build.directory}', property = "outputDir", required = true )
     private File outputDirectory;
 
     public void execute()
-        throws MojoExecutionException
+            throws MojoExecutionException
     {
         File f = outputDirectory;
 
@@ -59,7 +39,7 @@ public class MyMojo
         try
         {
             w = new FileWriter( touch );
-	    System.out.println("Creating 'touch.txt' file in the " + f + " directory.");
+            System.out.println("Creating 'touch.txt' file in the " + f + " directory.");
             w.write( "touch.txt" );
         }
         catch ( IOException e )
@@ -82,3 +62,4 @@ public class MyMojo
         }
     }
 }
+
