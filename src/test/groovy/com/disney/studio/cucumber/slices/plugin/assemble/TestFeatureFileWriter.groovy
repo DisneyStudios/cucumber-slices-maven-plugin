d8231e915277e70ac618182602f84d0111dfef85
Robert Stagner <robert.stagner@disney.com>
2016-10-04 14:37:39 -0700

    * starting my journey of writing some helpful unit tests

---
+++ src/test/groovy/com/disney/studio/cucumber/slices/plugin/assemble/TestFeatureFileWriter.groovy
@@ -0,0 +1,11 @@
+package com.disney.studio.cucumber.slices.plugin.assemble
+
+import org.junit.Test
+
+class TestFeatureFileWriter {
+
+    @Test
+    void shouldAlwaysPass() {
+        assert true
+    }
+}
---
+++ src/test/groovy/com/disney/studio/cucumber/slices/plugin/disassemble/TestFeatureFileCollector.groovy
@@ -0,0 +1,31 @@
+package com.disney.studio.cucumber.slices.plugin.disassemble
+
+import org.junit.Test
+
+class TestFeatureFileCollector {
+
+    @Test
+    void shouldAcceptFeaturePathInConstructor() {
+        String featuresDirPath = '/my/test/features/dir'
+        FeatureFileCollector featureFileCollector = new FeatureFileCollector(featuresDirPath)
+        assert featureFileCollector.dump().contains(featuresDirPath)
+    }
+
+    @Test(expected = IllegalArgumentException)
+    void shouldFailToGetCollectionIfMissingDirectory() {
+        String featuresDirPath = '/my/bogus/features/dir'
+        FeatureFileCollector featureFileCollector = new FeatureFileCollector(featuresDirPath)
+        featureFileCollector.getFeatureFileNameCollection()
+    }
+
+    @Test
+    void shouldReturnFeatureFileCollection() {
+        String featuresDirPath = 'src/it/simple-it/src/test/resources/features'
+        FeatureFileCollector featureFileCollector = new FeatureFileCollector(featuresDirPath)
+        List<String> featureFiles = featureFileCollector.getFeatureFileNameCollection()
+        assert featureFiles.size() > 0
+        assert featureFiles.every { fileName ->
+            fileName =~ /.*\.feature/
+        }
+    }
+}
