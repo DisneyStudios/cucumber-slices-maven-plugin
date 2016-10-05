b6b7ce8d43fa78c7002c840cbd0e5d93d22990cc
Robert Stagner <robert.stagner@disney.com>
2016-10-05 08:14:09 -0700

    * setup git to ignore the '/src/test/resources/features' directory, since the contents of the test file within that dir will vary * finished unit tests for TestFeatureFileWriter

--- src/test/groovy/com/disney/studio/cucumber/slices/plugin/assemble/TestFeatureFileWriter.groovy
+++ src/test/groovy/com/disney/studio/cucumber/slices/plugin/assemble/TestFeatureFileWriter.groovy
@@ -4,8 +4,6 @@ import groovy.io.FileType
 import org.junit.BeforeClass
 import org.junit.Test
 
-import java.nio.file.Paths
-
 class TestFeatureFileWriter {
     private static FeatureFileWriter featureFileWriter
     private static File featurePath
@@ -14,9 +12,14 @@ class TestFeatureFileWriter {
     @BeforeClass
     static void setup() {
         featureFileWriter = new FeatureFileWriter()
-        featurePath = Paths.get('src/test/resources/features').toFile()
-        featureFile = Paths.get('src/test/resources/features/test.feature').toFile()
+        featurePath = new File('src/test/resources/features')
+        if (!featurePath.exists()) {
+            featurePath.mkdir()
+        }
+        featureFile = new File('src/test/resources/features/test.feature')
+    }
 
+    private static void removeAllFeatureFiles() {
         // remove all feature files
         featurePath.eachFileRecurse(FileType.FILES) { file ->
             if (file.name =~ /.*\.feature/) {
@@ -25,18 +28,56 @@ class TestFeatureFileWriter {
         }
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException)
     void shouldFailDueToMissingFileHandle() {
         List<String> featureInformation = ['Feature: My Test Feature', 'Description of feature']
+        featureFileWriter.file = null
         featureFileWriter.writeFeature([featureInformation])
     }
 
     @Test
     void shouldWriteFeatureFileNarrative() {
-        List<String> featureInformation = ['Feature: My Test Feature', "As a casual user\nI want the ability to use Google's search feature\nSo that I can retrieve useful and accurate information"]
-        featureFileWriter.file = featureFile
-        featureFileWriter.writeFeature(featureInformation)
-        println ''
+        removeAllFeatureFiles()
 
+        List<String> featureInformation = ['Feature: My Test Feature'
+                                           , "As a casual user\nI want the ability to use Google's search feature\nSo that I can retrieve useful and accurate information"]
+        featureFileWriter.file = featureFile
+        featureFileWriter.writeFeature([featureInformation])
+
+        assert featureFile.text == """\
+Feature: My Test Feature
+As a casual user
+I want the ability to use Google's search feature
+So that I can retrieve useful and accurate information
+
+"""
+    }
+
+    @Test
+    void shouldWriteFeatureFileNarrativeBackgroundGeneralSteps() {
+        removeAllFeatureFiles()
+
+        List<String> featureInformation = ['Feature: Searching Google Using Background']
+        List<String> backgroundInformation = ['Background: Initial step', 'Given I am on Google home page']
+        List<String> generalStepsInformation = ['@smoke @regression', 'Scenario: The search for Java'
+                                                , 'When I enter the keyword of "Java"', 'And click the Submit button'
+                                                , 'Then the page title returned is "Java - Google Search"']
+
+        featureFileWriter.file = featureFile
+        featureFileWriter.writeFeature([featureInformation, backgroundInformation, generalStepsInformation])
+
+        assert featureFile.text == """\
+Feature: Searching Google Using Background
+
+Background: Initial step
+Given I am on Google home page
+
+@smoke @regression
+Scenario: The search for Java
+When I enter the keyword of "Java"
+And click the Submit button
+Then the page title returned is "Java - Google Search"
+
+"""
     }
 }
