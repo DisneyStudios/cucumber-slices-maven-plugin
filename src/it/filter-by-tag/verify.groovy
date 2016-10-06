2d9404a14ab61d92e8fc8dbbf461d90a26f43c78
STAGR005 <Robert.Stagner@disney.com>
2016-10-06 13:55:11 -0700

    * fixed 'filter by tag' integration test * fixed 'missing feature file' integration test

--- src/it/filter-by-tag/verify.groovy
+++ src/it/filter-by-tag/verify.groovy
@@ -20,6 +20,7 @@ I want the ability to use Google's search feature
 So that I can retrieve useful and accurate information
 
 
+@test-tag
 Scenario: The search for cheese
 Given I am on Google home page
 When I enter the keyword of "Cheese"
