cd996df63818a09f1048eb05f8b185aea204897a
Robert Stagner <robert.stagner@disney.com>
2016-10-06 10:13:29 -0700

    * modified the 'empty-feature-file' test

--- src/it/empty-feature-file/verify.groovy
+++ src/it/empty-feature-file/verify.groovy
@@ -1,4 +1,8 @@
 
 File runner = new File(basedir, 'target/it/empty-feature-file/src/test/groovy/parallel_runners/ParallelRunner0.groovy')
+File logFile = new File(basedir, 'build.log')
 
+String content = logFile.text
 assert !runner.exists()
+assert content.contains('[WARNING] Skipping feature file [A Feature File Without Scenarios] because it does not contain any scenarios!')
+assert content.contains('[WARNING] Missing generated Cucumber Runner files!! Skipping creation of Cucumber Runner files in the src/test/groovy/parallel_runners directory.')
