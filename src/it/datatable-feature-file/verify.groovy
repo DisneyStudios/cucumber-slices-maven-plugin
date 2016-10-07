dd86b76903f701496aa73f2077e99eb1ea980393
STAGR005 <Robert.Stagner@disney.com>
2016-10-07 04:35:25 -0700

    * added 'data table feature file' integration test

---
+++ src/it/datatable-feature-file/verify.groovy
@@ -0,0 +1,31 @@
+import groovy.io.FileType
+
+File runner0 = new File(basedir, 'src/test/groovy/parallel_runners/ParallelRunner0.groovy')
+File parallelFeatures = new File(basedir, 'src/test/resources/parallel_features')
+
+assert runner0.exists()
+
+String fileContent0 = ''
+
+parallelFeatures.eachFileRecurse(FileType.FILES) { featureFile ->
+    if (featureFile.name.contains('the-search-for-star-wars')) {
+        fileContent0 = featureFile.text
+    }
+}
+
+assert fileContent0 == """\
+Feature: Searching Google Using Data Table:  The search for star wars
+
+
+
+@regression
+Scenario: The search for star wars
+Given I am on Google home page
+When I enter the keyword of "Star Wars"
+And click the Submit button
+Then the following page elements shall be displayed
+| News |
+| Images |
+| Videos |
+
+"""
